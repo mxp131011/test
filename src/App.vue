9589fec0d4f3d1decb42cfbe7dfcc11585d2a855
Mr.Ma <mxp131011@qq.com>
2022-01-29 10:08:08 +0800

    有

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>{{ aaa }}</div>
+    <el-config-provider :locale="zhCn" size="small" :button="config">
+        <RouterView></RouterView>
+    </el-config-provider>
 </template>
 
 <script setup>
     import { shallowReactive } from 'vue';
+    import zhCn from 'element-plus/lib/locale/lang/zh-cn';
     const config = shallowReactive({
-        aaa: 'asaaa'
+        autoInsertSpace: true
     });
 </script>
